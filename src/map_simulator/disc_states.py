a0b2081f25568587cc52a5312340ff0b6d9e6556
José Arce <joseab10@gmail.com>
2020-09-15 21:11:15 +0200

    Added documentation and cosmetic changes to disc_states.py.

--- src/map_simulator/disc_states.py
+++ src/map_simulator/disc_states.py
@@ -1,9 +1,10 @@
 from enum import Enum
 
-import operator
-
 
 class DiscreteStates(Enum):
+    """
+    Enum for possible discrete states such as undefined (division by 0), Uniform Distribution, etc.
+    """
 
     #   KEY   | Val |   Text     |    Color        |  Label
     UNDEFINED =  1  , 'undefined', 'lightsteelblue', 'Undef.'
@@ -11,6 +12,15 @@ class DiscreteStates(Enum):
     BIMODAL   =  3  , 'bimodal'  , 'mediumpurple'  , 'BiMod. {0, 1}'
 
     def __new__(cls, v, text, color, label):
+        """
+        Method for creating new Enum objects. Overridden to add attributes to each possible value.
+
+        :param v: (int) Numerical value to be assigned to the enum. Also to be used as the value for the map cells.
+        :param text: (string) Unused text to better describe the enum.
+        :param color: (string|matplotlib color) Color to be assigned to this value.
+        :param label: (string) Text to be displayed in the legend color bar.
+        """
+
         obj = object.__new__(cls)
         obj._value_ = v
         obj.text = text
@@ -20,31 +30,70 @@ class DiscreteStates(Enum):
 
     @classmethod
     def list_all(cls):
+        """
+        List all possible discrete states.
+
+        :return: (list) List of all defined discrete states.
+        """
         ds_list = [s for s in cls]
         return ds_list
 
     @classmethod
     def sort_ds_list(cls, ds_list):
+        """
+        Orders a given list of discrete states by their numerical value.
+
+        :param ds_list: (list) List of Discrete State values.
+
+        :return: (list) List of Discrete States ordered by value.
+        """
+
         ordered_ds_list = sorted(ds_list, key=lambda x : x.value)
         return ordered_ds_list
 
     @classmethod
     def get_colors(cls, ds_list):
+        """
+        Get the color values for a given list of discrete states. The list is first ordered to maintain consistency.
+
+        :param ds_list: (list) List of Discrete State values.
+
+        :return: (list) List of colors corresponding to the (ordered) given states.
+        """
+
         ds_list = cls.sort_ds_list(ds_list)
         return [state.color for state in ds_list]
 
     @classmethod
     def get_values(cls, ds_list):
+        """
+        Get the numeric values for a given list of discrete states. The list is first ordered to maintain consistency.
+
+        :param ds_list: (list) List of Discrete State values.
+
+        :return: (list) List of numeric values corresponding to the (ordered) given states.
+        """
+
         ds_list = cls.sort_ds_list(ds_list)
         return [state.value for state in ds_list]
 
     @classmethod
     def get_labels(cls, ds_list):
+        """
+        Get the text labels for a given list of discrete states. The list is first ordered to maintain consistency.
+
+        :param ds_list: (list) List of Discrete State values.
+
+        :return: (list) List of string labels corresponding to the (ordered) given states.
+        """
         ds_list = cls.sort_ds_list(ds_list)
         return [state.label for state in ds_list]
 
 
 if __name__ == "__main__":
+    """
+    Test and sample code
+    """
 
     obj_type = type(DiscreteStates.UNIFORM)
     print('Type Test:', obj_type)
