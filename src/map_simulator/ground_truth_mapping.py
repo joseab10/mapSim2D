9b3558a3b67c036ae68d91906362fb7c2a0b1b8b
José Arce <joseab10@gmail.com>
2021-04-26 21:25:23 +0200

    Adding a feature for being able to run gt_mapper as a standalone program (without depending from SLAM).

--- src/map_simulator/ground_truth_mapping.py
+++ src/map_simulator/ground_truth_mapping.py
@@ -35,21 +35,38 @@ class GroundTruthMapping:
         self._tf_listener = tf.TransformListener()
 
         self._map_frame = tf_frame_normalize(rospy.get_param("~map_frame", "map"))
+        self._ref_frame = tf_frame_normalize(rospy.get_param("~ref_frame", "map"))
 
         max_scan_buffer_len = rospy.get_param("~max_scan_buffer_len", 1000)
         self._occ_threshold = rospy.get_param("~occ_threshold", 0.25)
 
+        self._standalone = rospy.get_param("~standalone", False)
+        map_resolution = rospy.get_param("~resolution", 0.05)
+
         self._sub_map = rospy.Subscriber("/map", OccupancyGrid, self._map_callback)
+        self._sub_eos = rospy.Subscriber("endOfSim", Bool, self._eos_callback)
+        self._gen_map = rospy.Subscriber("genMap", Bool, self._gen_map_callback)
         self._sub_scan = rospy.Subscriber("/GT/base_scan", LaserScan, self._sensor_callback)
-        self._sub_doLoc = rospy.Subscriber("/doLocOnly", Bool, self._loc_only_callback)
+        self._sub_doLoc = rospy.Subscriber("doLocOnly", Bool, self._loc_only_callback)
         self._pub_map = rospy.Publisher("/GT/map", OccupancyGrid, queue_size=1)
 
-        self._map_resolution = None
+        if self._standalone:
+            self._map_resolution = map_resolution
+        else:
+            self._map_resolution = None
 
         self._scan_buffer = deque(maxlen=max_scan_buffer_len)
 
         self._map_hits = defaultdict(int)
         self._map_visits = defaultdict(int)
+        self._width = 0
+        self._height = 0
+        self._min_x = None
+        self._min_y = None
+        self._max_x = None
+        self._max_y = None
+        self._map_origin = np.zeros(2)
+        self._msg_seq = 0
 
         self._loc_only = False
 
@@ -74,9 +91,9 @@ class GroundTruthMapping:
             return
 
         try:
-            self._tf_listener.waitForTransform(self._map_frame, msg.header.frame_id, msg.header.stamp,
-                                               rospy.Duration(2))
-            laser_pose, laser_orientation = self._tf_listener.lookupTransform(self._map_frame, msg.header.frame_id,
+            # self._tf_listener.waitForTransform(self._ref_frame, msg.header.frame_id, msg.header.stamp,
+            #                                    rospy.Duration(1))
+            laser_pose, laser_orientation = self._tf_listener.lookupTransform(self._ref_frame, msg.header.frame_id,
                                                                               msg.header.stamp)
 
         except (tf.LookupException, tf.ConnectivityException,
@@ -101,7 +118,7 @@ class GroundTruthMapping:
         endpoints = np.multiply(ranges.reshape((-1, 1)), cos_sin)
         endpoints += lp
 
-        if self._map_resolution is None:
+        if self._map_resolution is None or self._standalone:
             # Store the scan data until we receive our first map message and thus know the map's resolution
             self._scan_buffer.append((lp, endpoints, max_range))
         else:
@@ -109,6 +126,28 @@ class GroundTruthMapping:
 
         rospy.loginfo("Scan {} received and added to buffer at pose ({}): ({:.3f}, {:.3f}), theta: {:.3f}.".format(
             msg.header.seq, msg.header.frame_id, lp[0], lp[1], lp_th))
+
+    def _min_max_cells(self, cell):
+        c_x = cell[0]
+        c_y = cell[1]
+
+        if self._min_x is None:
+            self._min_x = c_x
+        elif c_x < self._min_x:
+            self._min_x = c_x
+        if self._max_x is None:
+            self._max_x = c_x
+        elif c_x > self._max_x:
+            self._max_x = c_x
+
+        if self._min_y is None:
+            self._min_y = c_y
+        elif c_y < self._min_y:
+            self._min_y = c_y
+        if self._max_y is None:
+            self._max_y = c_y
+        elif c_y > self._max_y:
+            self._max_y = c_y
 
     def _register_scan(self, laser_pose, endpoints, max_range):
         ilp = world2map(laser_pose, np.zeros(2), self._map_resolution)
@@ -129,6 +168,37 @@ class GroundTruthMapping:
             for visit in line_indexes:
                 visit_c = tuple(map2world(visit, np.zeros(2), self._map_resolution, rounded=True))
                 self._map_visits[visit_c] += 1
+                if self._standalone:
+                    self._min_max_cells(visit_c)
+
+    def _update_map_params(self):
+        if not self._standalone:
+            return
+
+        dims = world2map(np.array([self._max_x, self._max_y]), np.zeros(2), self._map_resolution) - \
+               world2map(np.array([self._min_x, self._min_y]), np.zeros(2), self._map_resolution)
+
+        self._height = dims[1] + 1
+        self._width = dims[0] + 1
+        self._map_origin = np.array([self._min_x, self._min_y])
+        self._msg_seq += 1
+        self._stamp = rospy.Time.now()
+
+    def _gen_map_callback(self, msg):
+        if not msg.data:
+            return
+
+        self._update_map()
+        self._publish_map()
+
+    def _eos_callback(self, msg):
+        if not msg.data:
+            return
+
+        self._update_map()
+        self._publish_map()
+
+        rospy.signal_shutdown("Received EndOfSimulation. Shutting Down")
 
     def _map_callback(self, msg):
         """
@@ -148,8 +218,8 @@ class GroundTruthMapping:
         """
 
         # Set map attributes
-        height = msg.info.height
-        width = msg.info.width
+        self._height = msg.info.height
+        self._width = msg.info.width
         if self._map_resolution is None:
             self._map_resolution = msg.info.resolution
 
@@ -157,16 +227,32 @@ class GroundTruthMapping:
             raise ValueError("Map resolution changed from last time {}->{}. I can't work in these conditions!".format(
                 self._map_resolution, msg.info.resolution))
 
-        map_origin = np.array([msg.info.origin.position.x, msg.info.origin.position.y])
+        self._map_origin = np.array([msg.info.origin.position.x, msg.info.origin.position.y])
+        self._msg_seq = msg.header.seq
+        self._stamp = msg.header.stamp
 
-        # For each scan in the measurement list, convert the endpoints to the center points of the grid cells,
-        # Get the cells crossed by the beams and mark those indexes as occ or free.
+        self._update_map()
+        self._publish_map()
+
+    def _update_map(self):
+        """
+        Function for converting the endpoints to the center points of the grid cells, computing the cells crossed by
+        the beams and mark those indexes as occ or free in the grid map, for every received scan still waiting
+        to be processed in the buffer.
+
+        :return: None
+        """
+
         while self._scan_buffer:
             laser_pose, endpoints, max_range = self._scan_buffer.popleft()
             self._register_scan(laser_pose, endpoints, max_range)
 
+        self._update_map_params()
+
+    def _publish_map(self):
+
         # Compute Occupancy value as hits/visits from the default dicts
-        map_shape = (width, height)
+        map_shape = (self._width, self._height)
         occ_map = -1 * np.ones(map_shape, dtype=np.int8)
 
         # Freeze a snapshot (copy) of the current hits and visits in case a scan message comes in and alters the values.
@@ -176,9 +262,9 @@ class GroundTruthMapping:
             if visits <= 0:
                 continue
 
-            ix, iy = world2map(pos, map_origin , self._map_resolution)
+            ix, iy = world2map(pos, self._map_origin , self._map_resolution)
             # Ignore cells not contained in image
-            if ix > width or iy > height:
+            if ix >= self._width or iy >= self._height:
                 continue
 
             hits = hits_snapshot[pos]
@@ -190,10 +276,16 @@ class GroundTruthMapping:
 
         # Build Message and Publish
         map_msg = OccupancyGrid()
-        map_msg.header = msg.header
-        map_msg.info = msg.info
+        map_msg.header.frame_id = self._map_frame
+        map_msg.header.stamp = self._stamp
+        map_msg.header.seq = self._msg_seq
+        map_msg.info.resolution = self._map_resolution
+        map_msg.info.origin.position.x = self._map_origin[0]
+        map_msg.info.origin.position.y = self._map_origin[1]
+        map_msg.info.height = self._height
+        map_msg.info.width = self._width
         map_msg.data = np.ravel(np.transpose(occ_map)).tolist()
 
-        rospy.loginfo("Publishing map at {} with seq {}.".format(self._map_frame, msg.header.seq))
+        rospy.loginfo("Publishing map at {} with seq {}.".format(self._map_frame, self._msg_seq))
 
         self._pub_map.publish(map_msg)
